94c8b911fa52aeb707a271cada701d09484d7874
Y94411 <scottjason918@fullsail.edu>
2017-05-02 13:14:30 -0700

    first commit

---
+++ ChampionDetailMapper.swift
@@ -0,0 +1,9 @@
+//
+//  ChampionDetailMapper.swift
+//  League Champs
+//
+//  Created by Jason Scott on 4/29/17.
+//  Copyright © 2017 Jason Scott. All rights reserved.
+//
+
+import Foundation
---
+++ League Champs/ChampionMapper.swift
@@ -0,0 +1,9 @@
+//
+//  ChampionMapper.swift
+//  League Champs
+//
+//  Created by Jason Scott on 4/22/17.
+//  Copyright © 2017 Jason Scott. All rights reserved.
+//
+
+import Foundation
